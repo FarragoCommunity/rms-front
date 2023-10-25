3a14cacd3657bc688871b092beb2b8e196c1b086
new-user6665 <148001297+new-user6665@users.noreply.github.com>
2023-10-25 10:28:12 +0000

    WISE CHANGED

--- src/components/result/mobile/MoblileDownLoad.tsx
+++ src/components/result/mobile/MoblileDownLoad.tsx
@@ -18,21 +18,21 @@ export default function MobileDownLoad() {
                 <Link className="w-2/6 h-5/6 bg-gray-300 rounded-xl flex flex-col items-center justify-around leading-tight" href="https://docs.google.com/spreadsheets/d/1yzjfSJvtiSIPr1ozSLCLMhIgPPhY648bQ_LgMoGI9j4/edit?usp=sharing" passHref={true}>
                     <ExcelClassicIcon className="w-8 h-8 fill-secondary" />
                     <div>
-                        <h1 className="text-tn font-bold text-center">CLASS VISE RESULT</h1>
+                        <h1 className="text-tn font-bold text-center">CLASS WISERESULT</h1>
                         <h1 className="text-tn text-center">(SMS)</h1>
                     </div>
                 </Link>
                 <Link className="w-2/6 h-5/6 bg-gray-300 rounded-xl flex flex-col items-center justify-around leading-tight" href="https://docs.google.com/spreadsheets/d/1mNESBjbU3jeLlB2fXz7YlXY6CaGCh2zruDWmyiDSnns/edit?usp=sharing" passHref={true}>
                     <ExcelDarkIcon className="w-8 h-8 fill-secondary" />
                     <div>
-                        <h1 className="text-tn font-bold text-center">DEPT VISE RESULT</h1>
+                        <h1 className="text-tn font-bold text-center">DEPT WISERESULT</h1>
                         <h1 className="text-tn text-center">(SMS)</h1>
                     </div>
                 </Link>
                 <Link className="w-2/6 h-5/6 bg-gray-300 rounded-xl flex flex-col items-center justify-around leading-tight" href="https://docs.google.com/spreadsheets/d/1yzjfSJvtiSIPr1ozSLCLMhIgPPhY648bQ_LgMoGI9j4/edit?usp=sharing" passHref={true}>
                     <ExcelClassicIcon className="w-8 h-8 fill-secondary" />
                     <div>
-                        <h1 className="text-tn font-bold text-center">CLASS VISE RESULT</h1>
+                        <h1 className="text-tn font-bold text-center">CLASS WISERESULT</h1>
                         <h1 className="text-tn text-center">(CHS)</h1>
                     </div>
                 </Link>
