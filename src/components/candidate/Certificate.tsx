9f516efbdec5cf9edf7ccb0caf764507fd93ede1
MuhammedHashimT <hashimt4567@gmail.com>
2023-12-21 12:42:22 +0530

    Update certificate generation code

--- src/components/candidate/Certificate.tsx
+++ src/components/candidate/Certificate.tsx
@@ -11,7 +11,7 @@ interface Props {
 export default function Certificate(props: Props) {
   const downloadCertificate = async () => {
     const candidateName: any = props?.candidate?.name?.toUpperCase();
-    const candidatePosition = props?.programme.position?.name;
+    const candidatePosition = props?.programme.position?.name?.toUpperCase();
     const candidateGrade = props?.programme.grade?.name;
     const programName: any = props?.programme.programme?.name?.toUpperCase();
     const programCategory = props?.candidate?.category?.name;
@@ -22,9 +22,9 @@ export default function Certificate(props: Props) {
       programCategory,
       programName
     );
-    const doc = new jsPDF('landscape', 'mm', 'a4');
+    const doc = new jsPDF('landscape', 'px', 'a4');
     // Load Montserrat font
-    doc.addFont('/certificate/Mont-Regular.ttf', 'Mont', 'normal');
+    doc.addFont('/certificate/AnekMalayalam-Light.ttf', 'Anek Malayalam', 'normal');
 
     const pdfWidth = doc.internal.pageSize.getWidth();
     const pdfHeight = doc.internal.pageSize.getHeight();
@@ -33,16 +33,16 @@ export default function Certificate(props: Props) {
     [candidateName].forEach((a: any) => {
       doc.addPage('a4');
       // const backgroundImageUrl = `/certificate/${programCategory}.png`;
-      const backgroundImageUrl = `/certificate/certificate.png`;
+      const backgroundImageUrl = `/certificate/certificate.jpg`;
       // Add the background image
       doc.addImage(backgroundImageUrl, 'JPEG', 0, 0, pdfWidth, pdfHeight);
 
       // Set the font to Montserrat
-      doc.setFont('Mont');
+      doc.setFont('Anek Malayalam');
 
       // Add text and other content on top of the background image
       doc.setFontSize(15);
-      doc.setTextColor(0, 0, 0); // Set text color to black
+      doc.setTextColor(200); // Set text color to black
 
 
 
@@ -68,29 +68,25 @@ export default function Certificate(props: Props) {
       // // Draw a line under specific words
       // doc.line(lineStartX + 4.5, yPosition + 2, lineEndX, yPosition + 2);
 
-
+      // This is to certify that Mr.Muhammed Jasil has been awarded the FIRST prize with A grade in EXCEL EXPERT SENIOR in Realia'23 DHIU UG Arts Fest organized by Al Huda Students' Association (ASAs) on October 11-15, 2023, at Darul Huda Islamic University.
       
-      const position =
-        candidatePosition == 'First'
-          ? '1st'
-          : candidatePosition == 'Second'
-          ? '2nd'
-          : '3rd';
+
       const lines = {
-        1: `This is to certify that Miss ${candidateName}`,
-        2: `has been awarded ${position} prize ${
-          candidateGrade ? `with ${candidateGrade} grade in` : 'only'
-        }`,
-        3: `${programName} (${programCategory}) in TEKTON'23, ZAHRA Arts fest`,
-        4: `organized by Girls Association of Zahra for Valuable Activities(GAZVA),`,
-        5: `on October 21-26 2023 at Fathima Zahra Islamic Womens' College.`,
+        1: `This is to certify that Mr .${candidateName} has been awarded`,
+        2: `${candidatePosition? `the ${candidatePosition} prize with `:''}${candidateGrade?`${candidateGrade} grade `:''}in ${programName} in Realia'23`,
+        3: `DHIU UG Arts Fest organized by Al Huda Students' Association (ASAs)`,
+        4: `on October 11-15, 2023, at Darul Huda Islamic University.`,
       };
-      doc.text(lines[1], pdfWidth / 2, 118, { align: 'center' });
+      // doc.text(lines[1], pdfWidth / 2, 118, { align: 'center' });
       // doc.line(((pdfWidth-doc.getStringUnitWidth(candidateName) * 15)/2) + 4.5, 118 + 2, doc.getStringUnitWidth(candidateName) * 15, 118 + 2);
-      doc.text(lines[2], pdfWidth / 2, 127, { align: 'center' });
-      doc.text(lines[3], pdfWidth / 2, 136, { align: 'center' });
-      doc.text(lines[4], pdfWidth / 2, 145, { align: 'center' });
-      doc.text(lines[5], pdfWidth / 2, 154, { align: 'center' });
+      doc.text(lines[1],145, 267.5);
+     doc.text(lines[2],145, 282.5);
+      doc.text(lines[3],145, 297.5);
+      doc.text(lines[4],145, 312.5);
+      // doc.text(lines[2], pdfWidth / 2, 127, { align: 'center' });
+      // doc.text(lines[3], pdfWidth / 2, 136, { align: 'center' });
+      // doc.text(lines[4], pdfWidth / 2, 145, { align: 'center' });
+      // doc.text(lines[5], pdfWidth / 2, 154, { align: 'center' });
     });
     doc.deletePage(1);
 
